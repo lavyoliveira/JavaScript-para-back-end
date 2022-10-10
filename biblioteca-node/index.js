1c8ba9fc555efa44cadd1ffe904a7503036760ef
Lavynia de Oliveira <lavynia.oliveira@unifei.edu.br>
2022-10-10 15:10:54 -0300

    async ... await

--- biblioteca-node/index.js
+++ biblioteca-node/index.js
@@ -1,17 +1,28 @@
-import fs from 'fs';
-import chalk from 'chalk';
+import fs from "fs";
+import chalk from "chalk";
 
 function trataErro(erro) {
-    console.log(erro);
-    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, "não há arquivo no diretório"));
 }
 
-function pegaArquivo(caminho){
-    const encode = 'utf-8';
-    fs.promises
-    .readFile(caminho, encode)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch(trataErro);
+async function pegaArquivo(caminho) {
+  try {
+    const encode = "utf-8";
+    const texto = await fs.promises.readFile(caminho, encode);
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro);
+  }
 }
 
-pegaArquivo('./arquivos/');
+// function pegaArquivo(caminho){
+//     const encode = 'utf-8';
+//     fs.promises
+//     .readFile(caminho, encode)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErro);
+// }
+
+pegaArquivo("./arquivos/texto.md");
+pegaArquivo("./arquivos/");
