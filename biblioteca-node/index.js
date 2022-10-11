d4af935c85f36438869ded536899bf2b0525a6f9
Lavynia de Oliveira <lavynia.oliveira@unifei.edu.br>
2022-10-11 14:50:20 -0300

    Regex texto

--- biblioteca-node/index.js
+++ biblioteca-node/index.js
@@ -1,6 +1,13 @@
 import fs from "fs";
 import chalk from "chalk";
 
+function extraiLinks(texto){
+  const regex = /\[[^[\]]*?\]\(https?:\/\/[^\s?#.].[^\s]*\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[0]]: captura[0]}))
+  return resultados;
+}
+
 function trataErro(erro) {
   console.log(erro);
   throw new Error(chalk.red(erro.code, "não há arquivo no diretório"));
@@ -10,13 +17,10 @@ async function pegaArquivo(caminho) {
   try {
     const encode = "utf-8";
     const texto = await fs.promises.readFile(caminho, encode);
-    console.log(chalk.green(texto));
+    console.log(extraiLinks(texto));
   } catch (erro) {
     trataErro(erro);
   }
 }
 
 pegaArquivo("./arquivos/texto.md");
-// pegaArquivo("./arquivos/");
-
-// \[[^[\]]*?\]
